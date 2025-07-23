f5ac1c6fd8fb4902ddee8dcfa8691c9e18092f9d
Shay Keren <shay.keren@gmail.com>
2025-07-23 17:13:48 +0300

    feat: optimize vet specialties loading with entity graph

--- src/main/java/org/springframework/samples/petclinic/vet/VetRepository.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetRepository.java
@@ -20,9 +20,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collection;
+import org.springframework.transaction.annotation.Transactional;import java.util.Collection;
 
 /**
  * Repository class for <code>Vet</code> domain objects All method names are compliant
@@ -34,13 +32,13 @@ import java.util.Collection;
  * @author Juergen Hoeller
  * @author Sam Brannen
  * @author Michael Isvy
- */
-public interface VetRepository extends Repository<Vet, Integer> {
+ */public interface VetRepository extends Repository<Vet, Integer> {
 
 	/**
 	 * Retrieve all <code>Vet</code>s from the data store.
 	 * @return a <code>Collection</code> of <code>Vet</code>s
 	 */
+	@EntityGraph(attributePaths = {"specialties"})
 	@Transactional(readOnly = true)
 	@Cacheable("vets")
 	Collection<Vet> findAll() throws DataAccessException;
@@ -51,6 +49,7 @@ public interface VetRepository extends Repository<Vet, Integer> {
 	 * @return
 	 * @throws DataAccessException
 	 */
+	@EntityGraph(attributePaths = {"specialties"})
 	@Transactional(readOnly = true)
 	@Cacheable("vets")
 	Page<Vet> findAll(Pageable pageable) throws DataAccessException;
