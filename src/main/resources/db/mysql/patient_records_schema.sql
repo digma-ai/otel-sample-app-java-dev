5a297cca5206b512d5d4e1c0d9b9b7b6e45dcde1
Shay Keren <shay.keren@gmail.com>
2025-07-24 14:45:55 +0300

    Add index on patient_weight column for better query performance

--- src/main/resources/db/mysql/patient_records_schema.sql
+++ src/main/resources/db/mysql/patient_records_schema.sql
@@ -12,5 +12,6 @@ CREATE TABLE patient_records (
     
     INDEX idx_treatment_type (treatment_type),
     INDEX idx_visit_date (visit_date),
-    INDEX idx_treatment_completed (treatment_completed)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
+    INDEX idx_treatment_completed (treatment_completed),
+    INDEX idx_patient_weight (patient_weight)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
