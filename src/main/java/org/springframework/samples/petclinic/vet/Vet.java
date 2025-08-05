9358dc6a2c08adbaedb76ea6d6d9898f7b707f79
Shay Keren <shay.keren@gmail.com>
2025-08-05 09:52:49 +0300

    Fix import formatting and add missing BatchSize import

--- src/main/java/org/springframework/samples/petclinic/vet/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vet.java
@@ -19,17 +19,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import org.springframework.beans.support.MutableSortDefinition;
+import java.util.Set;import org.springframework.beans.support.MutableSortDefinition;
 import org.springframework.beans.support.PropertyComparator;
-import org.springframework.samples.petclinic.model.Person;import jakarta.persistence.Entity;
+import org.springframework.samples.petclinic.model.Person;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.xml.bind.annotation.XmlElement;
+import org.hibernate.annotations.BatchSize;
 
 /**
  * Simple JavaBean domain object representing a veterinarian.
@@ -40,15 +40,14 @@ import jakarta.xml.bind.annotation.XmlElement;
  * @author Arjen Poutsma
  */
 @Entity
-@Table(name = "vets")public class Vet extends Person {
+@Table(name = "vets")
+public class Vet extends Person {
 
 	@BatchSize(size = 10)
 	@ManyToMany(fetch = FetchType.LAZY)
 	@JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
 			inverseJoinColumns = @JoinColumn(name = "specialty_id"))
-	private Set<Specialty> specialties;
-
-	protected Set<Specialty> getSpecialtiesInternal() {
+	private Set<Specialty> specialties;protected Set<Specialty> getSpecialtiesInternal() {
 		if (this.specialties == null) {
 			this.specialties = new HashSet<>();
 		}
